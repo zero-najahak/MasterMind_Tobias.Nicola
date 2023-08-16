d98b9eba297e98cf3d62d5d389b69fa6e6754961
tobiasclausen <tobias.clausen06@gmail.com>
2023-08-16 13:55:26 +0200

    Funktioniert alles

--- src/ch/css/mastermind/Main.java
+++ src/ch/css/mastermind/Main.java
@@ -8,47 +8,40 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-
-        //variabeln definieren
-        boolean run = true;
-        boolean wait = true;
-        int samecolour = 0;
-        boolean numbergenerator = true;
-        boolean korrekteeingabe = false;
-        boolean wiederholen = true;
+        boolean wiederholen;
+        boolean run;
+        boolean wait;
+        int samecolour;
+        boolean numbergenerator;
+        boolean korrekteeingabe;
         int richtigePosition = 0;
         int Rundenzaehler = 1;
+        boolean kompletterunde = false;
 
 
-        ArrayList<String> Position = new ArrayList<>();
-
-        //farbzuweisung
-
-        ArrayList<String> Colour = new ArrayList<>();
-        Colour.add("blau");
-        Colour.add("gelb");
-        Colour.add("rot");
-        Colour.add("pink");
-        Colour.add("gruen");
-        Colour.add("violett");
-        Colour.add("weiss");
-        Colour.add("schwarz");
-
-        ArrayList<String> NewPosition = new ArrayList<>();
-        ArrayList<String> Guess = new ArrayList<>();
-
-
-        //farbenkombination erstellen
         do {
+
+            ArrayList<String> Position = new ArrayList<>();
+
+
+            ArrayList<String> Colour = new ArrayList<>();
+            Colour.add("blau");
+            Colour.add("gelb");
+            Colour.add("rot");
+            Colour.add("pink");
+            Colour.add("grün");
+            Colour.add("violett");
+            Colour.add("weiss");
+            Colour.add("schwarz");
+
+            ArrayList<String> NewPosition;
+            ArrayList<String> Guess = new ArrayList<>();
             wiederholen = false;
             wait = true;
+            numbergenerator = true;
             do {
                 Random rand = new Random();
-                // Setting the upper bound to generate the
-                // random numbers in specific range
                 int upperbound = 8;
-                // Generating random values from 0 - 8
-                // using nextInt()
 
                 ArrayList<Integer> random = new ArrayList<>();
 
@@ -65,24 +58,12 @@ public class Main {
                     random.add(randomNumber);
                 }
 
-                //random zahl
-                for (int i = 0; i < 4; i++) {
-                    System.out.println(random.get(i));
-                }
-
-
-                //zahlen zu farben umwandeln
-                for (int i = 0; i < 4; i++) {
-                    System.out.println(Colour.get(random.get(i)));
-                }
                 for (int i = 0; i < 4; i++) {
                     Position.add(Colour.get(random.get(i)));
                 }
                 if (Position.size() == 4) {
                     numbergenerator = false;
                 }
-
-                System.out.println(Position);
 
             } while (numbergenerator);
 
@@ -91,35 +72,39 @@ public class Main {
 
             Scanner input = new Scanner(System.in);
 
-            System.out.println("Drüche s für Start ");
-            System.out.println("Drücke i für Info");
+            if (Rundenzaehler < 2 && !kompletterunde){
+                System.out.println("Drücke s für Start ");
+                System.out.println("Drücke i für Info");
+            }
+            System.out.println(Position);
+
             run = true;
             wait = true;
 
 
             while (run) {
 
-                while (wait) {
+                while (wait && Rundenzaehler < 2 && !kompletterunde) {
 
                     String key = input.nextLine();
-                    System.out.println(key);
 
                     if (key.equals("s")) {
 
                         wait = false;
 
-                    }
-
-                    if (key.equals("i")) {
+                    }else if (key.equals("i")) {
                         System.out.println("Drücke i für infos und s zum starten");
-                        System.out.println("Es werden 12 Spielrundenen gespielt, jede Runde hast du 4 guesses.");
+                        System.out.println("Es werden 12 Spielrundenen gespielt, jede Runde hast du 4 Versuche.");
                         System.out.println("Du musst die richtige Farbe und die richtige Position erraten, wenn die Position stimmt wird die richtige Farbe nicht noch einmal angegeben.");
-                        System.out.println("Zu verfügung stehen die Farben: gelb, blau, pink, rot,schwarz, weiss, violett und gruen.");
-                        System.out.println("Der Code ist völlig Random und besteht aus Vier verschiedene Farben, es kannen auch eine Farbe doppelt vorkommen.");
+                        System.out.println("Zu verfügung stehen die Farben: gelb, blau, pink, rot, schwarz, weiss, violett und grün.");
+                        System.out.println("Der Code ist völlig Random und besteht aus vier verschiedene Farben, es kannen auch eine Farbe doppelt vorkommen.");
                         System.out.println("Mit s kannst du das Spiel starten.");
-                        System.out.println("Du darfts kein Leerschlag bei der Eingabe machen.");
 
 
+                    }else{
+                        System.out.println("Diese taste hat keine Funktion");
+                        System.out.println("Drüche s für Start ");
+                        System.out.println("Drücke i für Info");
                     }
                 }
 
@@ -127,7 +112,7 @@ public class Main {
                     korrekteeingabe = false;
                     while (!korrekteeingabe) {
                         System.out.println(i + 1 + ". Versuch");
-                        String in = input.nextLine();
+                        String in = input.nextLine().trim().toLowerCase();
                         if (Colour.contains(in)) {
                             korrekteeingabe = true;
                             Guess.add(in);
@@ -140,92 +125,66 @@ public class Main {
                     }
                 }
 
+                NewPosition = new ArrayList<>(Position);
 
 
-
-
-
-
-
-
-
-
-                    //update zu urspünglichen farbe
-                    NewPosition = new ArrayList<>(Position);
-
-
-                    for (int i = 0; i < 4; i++) {
-                        System.out.println(NewPosition);
-                        if (Guess.get(i).equals(NewPosition.get(i))) {
-                            ++richtigePosition;
-                            NewPosition.set(i, "nothing");
-                        }
-
+                for (int i = 0; i < 4; i++) {
+                    if (Guess.get(i).equals(NewPosition.get(i))) {
+                        ++richtigePosition;
+                        NewPosition.set(i, "nothing");
                     }
-                    int fall = 0;
-                    System.out.println("guess");
-                    System.out.println(Guess);
+                }
+                int fall = 0;
 
-                    for (int i = 0; i < 4; ++i) {
-                        for (int d = 0; d < 4; ++d) {
-                            if (Guess.get(i).equals(NewPosition.get(d))) {
-                                ++fall;
-                                System.out.println(richtigePosition);
-                                NewPosition.set(d, "no");
-                            }
+
+                for (int i = 0; i < 4; ++i) {
+                    for (int d = 0; d < 4; ++d) {
+                        if (Guess.get(i).equals(NewPosition.get(d))) {
+                            ++fall;
+                            NewPosition.set(d, "no");
                         }
                     }
+                }
 
 
-                    // Richtige Farben vergeichen
+                System.out.println("Anzahl richtige Farben:" + (fall));
+                System.out.println("Anzahl richtige Positionen: " + richtigePosition);
+                System.out.println("Anzahl Runden Gespielt: " + Rundenzaehler);
+                ++Rundenzaehler;
 
-
-                    System.out.println("Anzahl richtige Farben:" + ((fall) - (richtigePosition)));
-                    System.out.println("Anzahl richtige Positionen: " + richtigePosition);
-                    System.out.println("Anzahl Runden Gespielt: " + Rundenzaehler);
-                    ++Rundenzaehler;
-
-                    while(Guess.size() > 0) {
+                while(Guess.size() > 0) {
                     Guess.remove(0);
-                    }
+                }
+                wait = false;
 
-                    if (richtigePosition == 4) {
-                        System.out.println("Du hast Gewonnen, Glückwunsch :)");
-                        wait = true;
-                    }
+                if (richtigePosition == 4) {
+                    System.out.println("Du hast Gewonnen, Glückwunsch :)");
+                    wait = true;
+                }
 
-                    if (Rundenzaehler == 13) {
-                        System.out.println("Du hast Verloren :(");
-                        wait = true;
-                    }
-
-
-
-
-
-
+                if (Rundenzaehler == 13) {
+                    System.out.println("Du hast Verloren :(");
+                    wait = true;
+                }
 
                 while (wait) {
                     System.out.println("drücke n für noch eine Runde und e zum beenden");
-                    String key = input.nextLine();
+                    String key = input.nextLine().trim().toLowerCase();;
 
                     if (key.equals("e")) {
                         run = false;
                         wait = false;
                         wiederholen = false;
 
-                    }
+                    }else if (key.equals("n")) {
 
-                    if (key.equals("n")) {
-                        wait = false;
-                        Rundenzaehler = 1;
-                        wiederholen = true;
                         run = false;
-                        samecolour = 0;
-                        numbergenerator = true;
-                        korrekteeingabe = false;
-                        richtigePosition = 0;
+                        wait = false;;
+                        wiederholen = true;
                         Rundenzaehler = 1;
+                        kompletterunde=true;
+                    }else {
+                        System.out.println("Diese Taste hat keine Funktion");
                     }
                 }
                 richtigePosition = 0;
